fd3bf79b95a11a1b417dcf73c69055a1a7e95932
Rubek Maharjan <rubekmhzn7@gmail.com>
2026-01-04 18:20:27 +0545

    feat: terms of services and privacy policy page

---
+++ src/lib/filters.ts
@@ -0,0 +1,23 @@
+const COUNTRIES = [
+    "Australia",
+    "Canada",
+    "USA",
+    "United Kingdom",
+    "New Zealand",
+    "Japan",
+    "South Korea",
+    "Singapore",
+    "Germany",
+    "Europe"
+]
+
+const DEGREES = [
+    "Bachelors",
+    "Masters",
+    "PhD"
+]
+
+export {
+    COUNTRIES,
+    DEGREES
+}
